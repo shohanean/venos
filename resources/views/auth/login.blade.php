4b5cc1d476e0837dfddce0e6f39795b207bbe548
Rayan Hossain <hossain_shohan1@live.com>
2023-06-19 12:23:18 +0600

    changed to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
         <!--begin::Heading-->
         <div class="text-center mb-10">
             <!--begin::Title-->
-            <h1 class="text-dark mb-3">Sign In to {{ env('APP_NAME') }}</h1>
+            <h1 class="text-dark mb-3">Login to {{ env('APP_NAME') }}</h1>
             <!--end::Title-->
             <!--begin::Link-->
             <div class="text-gray-400 fw-bold fs-4">New Here?
